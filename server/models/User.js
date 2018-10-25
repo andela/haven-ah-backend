d3a97d85661495d0ed1f99e2d1a9fa9469791e44
TheoOkafor <31534129+TheoOkafor@users.noreply.github.com>
2018-10-25 11:50:15 +0100

    chore(test): Update app to run test on travisCI (#8)
    - Update travis.yml file
    - Configure 'npm start' to transpile app and run ES5
    - Refactor some migration and model files
    - Create sample test file
    - Refactor to capture PR comments
    
    [delivers #161342794]

--- server/models/User.js
+++ server/models/User.js
@@ -1,70 +1,71 @@
-module.exports = (sequelize, DataTypes) => {
-const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  facebook: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  google: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  twitter: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  bio: {
-    type: DataTypes.TEXT,
-    allowNull: true,
-  },
-  role: {
-    type: DataTypes.ENUM('user', 'admin'),
-    allowNull: false,
-  },
-  emailConfirmation: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  resetToken: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  isDeleted: {
-    type: DataTypes.BOOLEAN,
-    allowNull: true,
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    field: 'created_at',
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    field: 'updated_at',
-    name: 'updatedAt',
-  },
-  deletedAt: {
-    type: DataTypes.DATE,
-    field: 'deleted_at',
-  },
-});
+/* eslint no-unused-vars:0 */
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    facebook: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    google: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    twitter: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    role: {
+      type: DataTypes.ENUM('user', 'admin'),
+      allowNull: false,
+    },
+    emailConfirmation: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    resetToken: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    imageUrl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    isDeleted: {
+      type: DataTypes.BOOLEAN,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      field: 'updated_at',
+      name: 'updatedAt',
+    },
+    deletedAt: {
+      type: DataTypes.DATE,
+      field: 'deleted_at',
+    },
+  });
 
-User.associate = (models) => {
+  User.associate = (models) => {
   // associations can be defined here
-}
-return User;
+  };
+  return User;
 };
